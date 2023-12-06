5d087476f12c63593c2985a5fce27f9d6f96c9a4
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-12-06 11:30:40 +0200

    server routing

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require("./routes/home");
+var userRouter = require("./routes/user");
+var postRouter = require("./routes/post");
+var todoRouter = require("./routes/todo");
+var commentRouter = require("./routes/comment");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", homeRouter);
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
+app.use("/todo", todoRouter);
 
 module.exports = app;
